9e51b39d1be90263f70fc1a843382b97aada4a75
RichardRanft <vegassparkyrich@gmail.com>
2016-01-07 06:15:07 -0800

    Add project to repo

---
+++ FalloutPrefs/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FalloutPrefs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbxSourceINI = new System.Windows.Forms.TextBox();
+            this.ofdOpen = new System.Windows.Forms.OpenFileDialog();
+            this.tctrlSettings = new System.Windows.Forms.TabControl();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source INI file:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(376, 23);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbxSourceINI
+            // 
+            this.tbxSourceINI.Location = new System.Drawing.Point(12, 25);
+            this.tbxSourceINI.Name = "tbxSourceINI";
+            this.tbxSourceINI.Size = new System.Drawing.Size(358, 20);
+            this.tbxSourceINI.TabIndex = 2;
+            // 
+            // ofdOpen
+            // 
+            this.ofdOpen.Filter = "INI files|*.ini";
+            // 
+            // tctrlSettings
+            // 
+            this.tctrlSettings.Location = new System.Drawing.Point(12, 51);
+            this.tctrlSettings.Name = "tctrlSettings";
+            this.tctrlSettings.SelectedIndex = 0;
+            this.tctrlSettings.Size = new System.Drawing.Size(726, 476);
+            this.tctrlSettings.TabIndex = 3;
+            this.tctrlSettings.SelectedIndexChanged += new System.EventHandler(this.tctrlSettings_SelectedIndexChanged);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(457, 23);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(98, 23);
+            this.btnRestore.TabIndex = 4;
+            this.btnRestore.Text = "Restore Backup";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(663, 23);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 537);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.tctrlSettings);
+            this.Controls.Add(this.tbxSourceINI);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(765, 576);
+            this.Name = "Form1";
+            this.Text = "Fallout 4 Preferences";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbxSourceINI;
+        private System.Windows.Forms.OpenFileDialog ofdOpen;
+        private System.Windows.Forms.TabControl tctrlSettings;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ FalloutPrefs/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BasicSettings;
+
+namespace FalloutPrefs
+{
+    public partial class Form1 : Form
+    {
+        private CSettings m_iniFile;
+        private int m_selectedTabIndex;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            ofdOpen.Filter = "INI files|*.ini";
+            if (ofdOpen.ShowDialog() == DialogResult.OK)
+            {
+                tbxSourceINI.Text = ofdOpen.FileName;
+                backupSource(ofdOpen.FileName);
+                loadINI(ofdOpen.FileName);
+            }
+        }
+
+        private void backupSource(string fileName)
+        {
+            String target = fileName + ".bak";
+            try
+            {
+                File.Copy(fileName, target);
+            }
+            catch (Exception ex)
+            {
+                if (MessageBox.Show("Do you want to overwrite your backup?", "Overwrite?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    File.Copy(fileName, target, true);
+            }
+        }
+
+        private void loadINI(String filename)
+        {
+            m_iniFile = new CSettings(filename);
+            m_iniFile.LoadSettings();
+            tctrlSettings.Controls.Clear();
+
+            var list = m_iniFile.Attributes.Keys.ToList();
+            list.Sort();
+            foreach (String attrib in list)
+            {
+                Point topleft = new Point(4, 4);
+
+                if (m_iniFile.Attributes[attrib].Count < 1)
+                    continue;
+                TabPage page = new TabPage(attrib);
+                page.Name = attrib;
+                tctrlSettings.TabPages.Add(page);
+
+                Panel panel = new Panel();
+                panel.Dock = DockStyle.Fill;
+                panel.Name = "pnl" + attrib;
+                page.Controls.Add(panel);
+
+                ListBox listbox = new ListBox();
+                listbox.Name = "lbx" + attrib;
+                listbox.Width = 300;
+                listbox.Height = panel.Height - 8;
+                panel.Controls.Add(listbox);
+                listbox.Location = topleft;
+                List<String> boxItems = new List<String>();
+                foreach (String setting in m_iniFile.Attributes[attrib].Keys)
+                    boxItems.Add(setting);
+                boxItems.Sort();
+                foreach (String entry in boxItems)
+                    listbox.Items.Add(entry);
+                listbox.SelectedIndexChanged += listbox_SelectedIndexChanged;
+
+                topleft = new Point(listbox.Right + 4, listbox.Top);
+
+                // examine/edit
+                Label lbl = new Label();
+                lbl.Name = "lbl" + attrib;
+                lbl.Text = "Field Value:";
+                lbl.Width = 200;
+                lbl.TextAlign = ContentAlignment.MiddleLeft;
+                panel.Controls.Add(lbl);
+                lbl.Location = topleft;
+
+                topleft.Y += lbl.Height + 4;
+                TextBox box = new TextBox();
+                box.Width = 300;
+                box.Name = "tbx" + attrib;
+                panel.Controls.Add(box);
+                box.Location = topleft;
+
+                Button btn = new Button();
+                topleft.Y = (box.Top - ((btn.Height - box.Height) / 2));
+                topleft.X = box.Right + 4;
+                btn.Name = "btn" + attrib;
+                btn.Text = "Set";
+                btn.Click += button_Click;
+                panel.Controls.Add(btn);
+                btn.Location = topleft;
+
+                // add
+                Label lblAdd = new Label();
+                lblAdd.Name = "lblAdd" + attrib;
+                lblAdd.Text = "Add";
+                lblAdd.Width = 200;
+                lblAdd.TextAlign = ContentAlignment.MiddleLeft;
+                panel.Controls.Add(lblAdd);
+                topleft.X = lbl.Left;
+                topleft.Y = box.Top + box.Height + 4;
+                lblAdd.Location = topleft;
+
+                topleft.Y += lblAdd.Height + 4;
+                TextBox boxAddName = new TextBox();
+                boxAddName.Width = 200;
+                boxAddName.Name = "tbxAdd" + attrib + "Name";
+                panel.Controls.Add(boxAddName);
+                boxAddName.Location = topleft;
+
+                topleft.X += boxAddName.Width + 4;
+                TextBox boxAddVal = new TextBox();
+                boxAddVal.Width = 96;
+                boxAddVal.Name = "tbxAdd" + attrib + "Value";
+                panel.Controls.Add(boxAddVal);
+                boxAddVal.Location = topleft;
+
+                Button btnAdd = new Button();
+                topleft.Y = (boxAddName.Top - ((btnAdd.Height - boxAddName.Height) / 2));
+                topleft.X = boxAddVal.Right + 4;
+                btnAdd.Name = "btnAdd" + attrib;
+                btnAdd.Text = "Add";
+                btnAdd.Click += button_AddClick;
+                panel.Controls.Add(btnAdd);
+                btnAdd.Location = topleft;
+
+                m_selectedTabIndex = tctrlSettings.SelectedIndex;
+            }
+        }
+
+        private void listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            if (listBox.SelectedItem == null)
+                return;
+            String txtBoxName = "tbx" + tctrlSettings.TabPages[m_selectedTabIndex].Name;
+            String lblName = "lbl" + tctrlSettings.TabPages[m_selectedTabIndex].Name;
+            String setting = listBox.SelectedItem.ToString();
+            TextBox txtbox = (TextBox)tctrlSettings.TabPages[m_selectedTabIndex].Controls.Find(txtBoxName, true)[0];
+            Label lbl = (Label)tctrlSettings.TabPages[m_selectedTabIndex].Controls.Find(lblName, true)[0];
+            if (lbl != null)
+                lbl.Text = setting;
+            if (txtbox != null)
+                txtbox.Text = m_iniFile.Attributes[tctrlSettings.TabPages[m_selectedTabIndex].Name][setting].ToString();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            String listboxname = "lbx" + tctrlSettings.TabPages[m_selectedTabIndex].Name;
+            String txtboxname = "tbx" + tctrlSettings.TabPages[m_selectedTabIndex].Name;
+            ListBox lstBox = (ListBox)tctrlSettings.TabPages[m_selectedTabIndex].Controls.Find(listboxname, true)[0];
+            TextBox txtBox = (TextBox)tctrlSettings.TabPages[m_selectedTabIndex].Controls.Find(txtboxname, true)[0];
+            String attribute = tctrlSettings.TabPages[m_selectedTabIndex].Name;
+            String setting = "";
+            String value = "";
+            if (lstBox == null)
+                return;
+            setting = lstBox.Items[lstBox.SelectedIndex].ToString();
+            if (txtBox == null)
+                return;
+            value = txtBox.Text;
+
+            m_iniFile.Set(attribute, setting, value);
+        }
+
+        private void button_AddClick(object sender, EventArgs e)
+        {
+            String listboxname = "lbx" + tctrlSettings.TabPages[m_selectedTabIndex].Name;
+            String txtboxaddname = "tbxAdd" + tctrlSettings.TabPages[m_selectedTabIndex].Name + "Name";
+            String txtboxaddval = "tbxAdd" + tctrlSettings.TabPages[m_selectedTabIndex].Name + "Value";
+            ListBox lstBox = (ListBox)tctrlSettings.TabPages[m_selectedTabIndex].Controls.Find(listboxname, true)[0];
+            TextBox txtBoxAddname = (TextBox)tctrlSettings.TabPages[m_selectedTabIndex].Controls.Find(txtboxaddname, true)[0];
+            TextBox txtBoxAddvalue = (TextBox)tctrlSettings.TabPages[m_selectedTabIndex].Controls.Find(txtboxaddval, true)[0];
+            String attribute = tctrlSettings.TabPages[m_selectedTabIndex].Name;
+            String setting = "";
+            String value = "";
+            if (lstBox == null)
+                return;
+            if (txtBoxAddname == null)
+                return;
+            setting = txtBoxAddname.Text;
+            value = txtBoxAddvalue.Text;
+            m_iniFile.Add(attribute, setting, value);
+
+            lstBox.Items.Clear();
+            String[] array = new String[m_iniFile.Attributes[attribute].Keys.Count];
+            m_iniFile.Attributes[attribute].Keys.CopyTo(array, 0);
+            var list = array.ToList<String>();
+            list.Sort();
+            foreach (String key in list)
+                lstBox.Items.Add(key);
+        }
+
+        private void tctrlSettings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            m_selectedTabIndex = tctrlSettings.SelectedIndex;
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            ofdOpen.Filter = "BAK files|*.bak";
+            if (ofdOpen.ShowDialog() == DialogResult.OK)
+            {
+                String target = ofdOpen.FileName.Replace(".bak", "");
+                File.Copy(ofdOpen.FileName, target, true);
+                m_iniFile.LoadSettings();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(tbxSourceINI.Text))
+            {
+                m_iniFile.SaveSettings();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Form1 form = (Form1)sender;
+            Size clientSize = form.ClientSize;
+            btnSave.Left = clientSize.Width - btnSave.Width - 12;
+
+            tctrlSettings.Width = clientSize.Width - (2*tctrlSettings.Left);
+            tctrlSettings.Height = clientSize.Height - tctrlSettings.Top - 12;
+        }
+    }
+}
